8b09b2ace2cc8995e55bb9d20095db83f9f39368
Tharunkumar G <tharung1411@gmail.com>
2025-04-26 12:50:54 +0530

    code update

--- FlatteningNestedObject/index.js
+++ FlatteningNestedObject/index.js
@@ -31,3 +31,27 @@ function flatern(obj, parentKey) {
 
 const result = flatern(user, 'user');
 console.log(result)
+
+
+//method for remove duplicate
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5];
+let map = new Map();
+let removeDep = [];
+
+function removeDuplicate(arr){
+    for(let i=0; i<=arr.length-1; i++){
+        if(!map.has(arr[i])){
+            map.set(arr[i], 1);
+            removeDep.push(arr[i])
+        }else{
+            let count = map.get(arr[i])
+            map.set(arr[i], count+1);
+            if(map.get(arr[i]) === 1){
+                removeDep.push(arr[i])
+            }
+        }
+    }
+}
+
+removeDuplicate(arr);
+console.log(removeDep);
